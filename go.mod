939d286966fb54d52dba62ca6f71210bb4b70280
Thomas Santerre <thomas@santerre.xyz>
2019-05-09 20:27:10 -0400

    Remove vendor

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/coredns/coredns v1.5.0
 	github.com/mholt/caddy v1.0.0
-	github.com/miekg/dns v1.1.9 // indirect
+	github.com/miekg/dns v1.1.9
+	golang.org/x/net v0.0.0-20190328230028-74de082e2cca
 )
--- xpf.go
+++ xpf.go
@@ -2,6 +2,7 @@ package xpf
 
 import (
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
 	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 	"golang.org/x/net/context"
@@ -19,8 +20,11 @@ func New() (*XPF, error) {
 
 func (xpf *XPF) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	state := request.Request{W: w, Req: r}
-
+	rrw := dnstest.NewRecorder(w)
+	rc, err := plugin.NextOrFailure(xpf.Name(), xpf.Next, ctx, rrw, r)
 }
+
+func (xpf *XPF) Name() string { return "xpf" }
 
 // AppendXpfRecord adds the relevant XPF record to the request object
 func appendXpfRecord(*request.Request) error {
