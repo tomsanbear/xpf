7ba53758dd9cde29678b526a2e098b18c0e97d81
Thomas Santerre <thomas@santerre.xyz>
2019-05-23 21:36:25 -0400

    test ipv6 record addition

--- xpf_test.go
+++ xpf_test.go
@@ -7,33 +7,34 @@ import (
 	"testing"
 
 	"github.com/coredns/coredns/plugin/test"
+	coretest "github.com/coredns/coredns/plugin/test"
 	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAppendXpfRecord(t *testing.T) {
+func TestNew(t *testing.T) {
+	xpf, err := New()
+	assert.NotNil(t, xpf)
+	assert.NoError(t, err)
+}
+
+func TestAppendXpfRecordV4(t *testing.T) {
 	testrequest := new(dns.Msg)
 	testrequest.SetQuestion("example.org.", dns.TypeA)
-	teststate := request.Request{W: &test.ResponseWriter{}, Req: testrequest}
+	teststate := request.Request{W: &coretest.ResponseWriter{}, Req: testrequest}
+	err := appendXpfRecord(&teststate)
+	assert.NoError(t, err)
+	assert.True(t, containsXpfRR(teststate, XPFPrivateRR{4, 17, net.IPv4(10, 240, 0, 1).To4(), net.IPv4(127, 0, 0, 1).To4(), 40212, 53}), "failed to find an expected XPF Data object in additional section\n")
+}
 
-	tests := []struct {
-		testrecord  request.Request
-		expectedXPF XPFPrivateRR
-		shouldfail  bool
-	}{
-		{teststate, XPFPrivateRR{4, 17, net.IPv4(10, 240, 0, 1).To4(), net.IPv4(127, 0, 0, 1).To4(), 40212, 53}, false},
-	}
-
-	for _, test := range tests {
-		err := appendXpfRecord(&test.testrecord)
-		if test.shouldfail {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-			assert.True(t, containsXpfRR(test.testrecord, test.expectedXPF), "failed to find an expected XPF Data object in additional section")
-		}
-	}
+func TestAppendXpfRecordV6(t *testing.T) {
+	testrequest := new(dns.Msg)
+	testrequest.SetQuestion("example.org.", dns.TypeAAAA)
+	teststate := request.Request{W: &coretest.ResponseWriter6{}, Req: testrequest}
+	err := appendXpfRecord(&teststate)
+	assert.NoError(t, err)
+	assert.True(t, containsXpfRR(teststate, XPFPrivateRR{6, 17, net.ParseIP("fe80::42:ff:feca:4c65"), net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 40212, 53}), "failed to find an expected XPF Data object in additional section\n")
 }
 
 func BenchmarkAppendXpfRecord(b *testing.B) {
